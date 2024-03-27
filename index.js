6c1791112634392166a2108b5019a231b72a9f18
renanMeloDev <dev.renanmelo@gmail.com>
2024-03-27 16:51:52 -0300

    primeira tentativa

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 let hero ="punhos de ferrugem quântica"
 let XP = 1000
-switch (XP){ 
-    case XP <= 1000:
-        console.log(hero + "Tem caregoria Ferro")
-        break
+
+if (XP = 1000){
+    console.log(hero + " tem categoria ferro")
+}
+else if (XP >= 1001 || XP < 2000){
+    console.log(hero + " tem categoria prata")
+}else if (XP >=2001 || XP < 5000){
+    console.log(hero +" tem categoria ouro")
+}else if (XP >= 5001 || XP < 8000){
+    console.log( hero+ " tem categoria Platina Diamante")
+}else if (XP >= 8001 || XP < 9000){
+    console.log(hero+ " tem categoria Ascendente")
+}else if (XP >= 9001 || XP < 10000){
+    console.log(hero+ " tem categoria Imortal")
+}if (XP > 10001){
+    console.log(hero+ " tem categoria Radiante")
+}
     
 
-}
