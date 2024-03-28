2a23a27ea59f18949bb516fd48be321367aeb835
renanMeloDev <dev.renanmelo@gmail.com>
2024-03-28 12:10:05 -0300

    commit final

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-let hero ="punhos de ferrugem quântica"
-let XP = 1000
+let heroi ="punhos de ferrugem quântica"
+let XP = 10050;
+console.log(XP)
 
-if (XP = 1000){
-    console.log(hero + " tem categoria ferro")
+if (XP === 1000){
+    console.log(heroi+ "  categoria ferro")
+}else if (XP <= 1001){
+    console.log(heroi+ " categoria bronze")
+}else if (XP < 2000){
+    console.log(heroi+" categotia bronze")
+}else if (XP <= 2001){
+    console.log (heroi +" categoria prata")
+}else if (XP < 5000){
+    console.log(heroi+" categoria prata")
+}else if(XP<=5001){
+    console.log(heroi+" categoria ouro")
+}else if(XP<7000){
+    console.log(heroi+" categoria ouro")
+}else if(XP<=7001){
+    console.log(heroi+" categoria platina")
+}else if(XP<8000){
+    console.log(heroi+" categoria platina")
+}else if(XP<=8001){
+    console.log(heroi+" categoria ascendente")
+}else if(XP<9000){
+    console.log(heroi +" categoria Ascendente")
+}else if(XP<=9001){
+    console.log(heroi+" categoria Imortal")
+}else if(XP<10000){
+    console.log(heroi + " categoria Imortal")
+}else if (XP>=10001){
+    console.log(heroi +" categoria radiante")
 }
-else if (XP >= 1001 || XP < 2000){
-    console.log(hero + " tem categoria prata")
-}else if (XP >=2001 || XP < 5000){
-    console.log(hero +" tem categoria ouro")
-}else if (XP >= 5001 || XP < 8000){
-    console.log( hero+ " tem categoria Platina Diamante")
-}else if (XP >= 8001 || XP < 9000){
-    console.log(hero+ " tem categoria Ascendente")
-}else if (XP >= 9001 || XP < 10000){
-    console.log(hero+ " tem categoria Imortal")
-}if (XP > 10001){
-    console.log(hero+ " tem categoria Radiante")
-}
-    
+
+
+   
 
